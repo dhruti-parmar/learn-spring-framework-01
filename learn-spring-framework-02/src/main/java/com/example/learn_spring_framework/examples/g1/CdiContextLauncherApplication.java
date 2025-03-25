05bceaa12e5085468f324fa6df9fe7fcfb745cc3
dhruti-parmar <dhrutiaparmar111@gmail.com>
2025-03-25 12:18:52 +0530

    Add files via upload

---
+++ learn-spring-framework-02/src/main/java/com/example/learn_spring_framework/examples/g1/CdiContextLauncherApplication.java
@@ -0,0 +1,47 @@
+package com.example.learn_spring_framework.examples.g1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+//@Component
+@Named
+class BusinessService{
+	private DataService dataservice;
+
+	public DataService getDataservice() {
+		return dataservice;
+	}
+
+//	@Autowired
+	@Inject
+	public void setDataservice(DataService dataservice) {
+		System.out.println("Setter Injection");
+		this.dataservice = dataservice;
+	}
+}
+
+//@Component
+@Named
+class DataService{
+	
+}
+
+@Configuration
+@ComponentScan
+public class CdiContextLauncherApplication {
+	public static void main(String[] args) {
+		
+		try(var context =
+				new AnnotationConfigApplicationContext(CdiContextLauncherApplication.class)){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		
+			System.out.println(context.getBean(BusinessService.class).getDataservice());
+		}
+	} 
+}
